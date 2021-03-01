f97d25afeaa27121036968580f4b75e1db534cfc
huy <hoangconghuy36@gmail.com>
2021-03-01 15:58:22 +0700

    o

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,12 @@
-
-var mongoose=require("../config/dbConfig")
-var schema =mongoose.Schema
+var mongoose = require("../config/dbConfig")
+var schema = mongoose.Schema
 var userSchema = new schema({
-  email: String, 
-  username: String,
-  password:String,
-  phone: Number,
-  school: String
+    email: String,
+    username: String,
+    password: String,
+    phone: Number,
+    school: String
 })
-var userModel =mongoose.model("user",userSchema)
-module.exports=userModel
+var userModel = mongoose.model("user", userSchema)
+module.exports = userModel
+    // hh
